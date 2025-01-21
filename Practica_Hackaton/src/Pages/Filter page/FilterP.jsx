bf3c98cd2a9efe0aa816bbb802c51491cb07efdb
Alexander <alexanderaguilar726@gmail.com>
2025-01-20 21:31:55 -0600

    Estructura inicial

---
+++ Practica_Hackaton/src/Pages/Filter page/FilterP.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Filter from "../../components/Filter component/FilterC";
+
+const FilterP = () => {
+  return (
+    <div>
+      <Filter />
+    </div>
+  );
+};
+
+export default FilterP;
---
+++ Practica_Hackaton/src/components/Filter component/FilterC.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Filter = () => {
+  return (
+    <div>Hola Filtro</div>
+  )
+}
+
+export default Filter
