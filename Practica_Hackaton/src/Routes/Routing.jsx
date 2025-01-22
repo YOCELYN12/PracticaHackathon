53f1542cf55f6e77580bedcf5ba913e4804c53c7
Yos12 <yocelynrivera9@gmail.com>
2025-01-22 16:53:34 -0600

    Prueba

---
+++ Practica_Hackaton/src/Routes/Routing.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Filter from "../Pages/Filter page/FilterP";
+
+const Routing = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Filter />} />
+        </Routes>
+
+        
+      </Router>
+    </div>
+  );
+};
+
+export default Routing;
